0088fab0b1c45b8ef9ce32c389cf54aae48e831a
jero98772 <jero98772@gmail.com>
2020-01-29 16:17:37 -0500

    introduccion a ing sitemas

---
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/calculadora.py
@@ -0,0 +1,5 @@
+from math import e ,pi ,sqrt ,exp ,log , cos, sin, tan, atan, asin ,acos
+while True:
+    entrada=eval(input(" que quieres calcular \n interrimupa con ctr+c \n"))
+    print(entrada)
+
