6f2b8c475102482120691fcf75696f74992e0468
jero98772 <jero98772@gmail.com>
2020-01-29 19:15:55 -0500

    pesamiento algorimico y exresion escrita

---
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/ejercio1.py
@@ -0,0 +1,11 @@
+#ejercio 3
+#punto tercer 2
+Cantiadbod=100
+Unidadesven=1
+pedidos=1
+prueba=bool((Cantiadbod or Unidadesven )/ pedidos >=2)
+print(prueba)
+#punto tercer 3
+tipo=""
+valorUnitario=1
+print(bool(tipo=="DROGERIA" and 1000< valorUnitario<3500))
