64212864156505378d70b9f4436466352776c728
jero98772 <jero98772@gmail.com>
2020-02-02 22:46:54 -0500

    invirtiendo tiempo a algorimo y expresion escrita

--- clases/clase#_8__pesnamineto_algorimico__2020-01-20/talleres_con_arrays/pseudorandom.py
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/talleres_con_arrays/pseudorandom.py
@@ -6,24 +6,26 @@ X0 valor inicial
 X valor
 xn+1 = (axn + c) mod m
 """
-class random(valor = 3, mod = 600 ,incrementador = 10,multiplicador = 7 ,veces=1):
-        self.valor = valor #valor  inicial para ir aumentandolo y disminuyendo
-        self.mod = mod
-        self.incrementador = incrementador
-        self.multiplicador = multiplicador
-        self.veces = veces #veces para que salgan numeros "aleatorios" o tamaño del la lista 
-        self.array = []
-        def matrix(self):
+class random():
+        def __init__(self,valorInicial = 3, mod = 600 ,incrementador = 10,multiplicador = 7 ,veces=1):    
+            self.valorInicial = valorInicial #valor  inicial para ir aumentandolo y disminuyendo
+            self.mod = mod # tambien se pude usar como valor maximo
+            self.incrementador = incrementador
+            self.multiplicador = multiplicador
+            self.veces = veces #veces para que salgan numeros "aleatorios" o tamaño del la lista 
+            self.array = []
+        def vector(self):
                 self.contador = 0
+                self.valor=self.valorInicial 
                 while self.valor < self.mod and self.veces > self.contador:
 	                self.valor = (self.multiplicador * self.valor + self.incrementador) % self.mod
 	                self.contador += 1
 	                self.array.append(self.valor)    
                 return self.array
         def digito(self):
-	        self.digito = self.array[valor]
-                return self.digito
-	def help():
-                print("formula\n m modulo \n  a multiplicador \n c incremento \n X0 valor inicial \n X valor \n xn+1 = (axn + c) mod m")
-                print("m>0 \n  m>a>0 \n m>c>0")
+            self.digito = self.array[(self.multiplicador // self.valor + self.incrementador) % self.mod]
+            return self.digito
+        def help(self):
+                print("PSEUDORANDOM \n formula \n m modulo \n a multiplicador \n c incremento \n X0 valor inicial \n X valor \n xn+1 = (axn + c) mod m")
+                print("\n \n m>0 \n m>a>0 \n m>c>0")
 #continuara despues
