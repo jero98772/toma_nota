aa4659a0e97150136d2c24b8d0cf398d08bf0a2a
jero98772 <jero98772@gmail.com>
2020-02-03 11:49:10 -0500

    algorimto

--- clases/clase#_8__pesnamineto_algorimico__2020-01-20/talleres_con_arrays/almenos3veces.py
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/talleres_con_arrays/almenos3veces.py
@@ -1,4 +1,41 @@
 import pseudorandom as pserand
+import numpy as np
 
-rnd=pserand.random()
-rnd=rnd.help()
+cantidad = int(input("parar poc ctrl + c un cantiadad de veces a contar :  ")) #elemntos
+unaleatorio=1
+rnd=pserand.random(veces = cantidad, incrementador = 1, mod = cantidad)
+for i in range(cantidad): 
+    unaleatorio=rnd.digito()//3*unaleatorio
+    rnd=pserand.random(veces = cantidad, incrementador = unaleatorio, mod = cantidad)
+    
+array=rnd.vector()
+medida=len(array) #deberia ser igual al tamaño de cantiadad con el mismo numero de elementos
+if medida == cantidad:
+    print("si es es de mismo tamaño  de ",medida, "elementos")
+else :
+    print("son de difenete medida")
+randvect = rnd.vector()
+counter = 0
+size= medida
+rng=np.random.default_rng()
+vector=rng.choice(size,size=size,replace=False)
+elemenetosigules = []
+randvect=vector
+for j in randvect:#contar hatsta el  numero del input
+    for i in range(medida):
+        if randvect.index(i) > 3:
+             print("se supone que esta ssadsafdwfeabgvpfpmflnnwlfvnmvl")
+
+        #if  i == j:
+            #print(randvect)
+        """
+            if j == randvect[counter]:
+                print("if ",j," == vector[indice]",)
+                counter+=1
+                
+                numeroalmenos=randvect.index(i)
+                print("numero al menos",j,numeroalmenos)
+                elemenetosigules.append(numeroalmenos)
+                print(j,"lista",elemenetosigules) """
+        # if randvect.index(i) > 3:
+         #    print("se supone que esta ssadsafdwfeabgvpfpmflnnwlfvnmvl")
