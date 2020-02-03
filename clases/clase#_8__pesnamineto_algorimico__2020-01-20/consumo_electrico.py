9bab4759d4e0410dcbe731cf1b56860dae8ce14a
jero98772 <jero98772@gmail.com>
2020-02-03 17:20:22 -0500

    expresion escrita ,pensamiento aloritmico

--- clases/clase#_8__pesnamineto_algorimico__2020-01-20/consumo_electrico.py
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/consumo_electrico.py
@@ -4,3 +4,6 @@ consumo= vatios/personas
 print("vatios ",vatios ,"para ",personas,"personas y gastan",consumo)
 if consumo>2000/personas:
     print("uy heramno se paso de consumo")
+#modulos energia
+modulo= 220
+print("modulo",modulo,)
