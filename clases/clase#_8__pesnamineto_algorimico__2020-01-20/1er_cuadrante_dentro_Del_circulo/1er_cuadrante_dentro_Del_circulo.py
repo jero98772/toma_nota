986c46780208dd53472ca51a47338915ead82ada
jero98772 <jero98772@gmail.com>
2020-02-04 09:56:47 -0500

    organisando el repositorio

--- clases/clase#_8__pesnamineto_algorimico__2020-01-20/1er_cuadrante_dentro_Del_circulo/1er_cuadrante_dentro_Del_circulo.py
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/1er_cuadrante_dentro_Del_circulo/1er_cuadrante_dentro_Del_circulo.py
@@ -1,11 +1,9 @@
-from PI import pi
-pi = pi.pi()
 radio = int(input("radio: "))
-X = int(input("cordenda del punto X"))
-Y = int(input("cordenda del punto Y"))
-UbicacionPunto = (((X**2)+(Y**2))**(1/2))
-print(UbicacionPunto)
-#area = pi*(radio**2)
-#areaCuadrante= area/4
-#print("area",area)
-#print("area por Cuadrante",areaCuadrante)
+X = int(input("cordenda del punto X: "))
+Y = int(input("cordenda del punto Y: "))
+DistanciaPunto = (((X**2)+(Y**2))**(1/2))
+print("distancia del punto es",DistanciaPunto)
+if radio >= DistanciaPunto and X>0 and Y>0:
+    print("esta dentro del circulo y esta en el cuadrante 1")
+else:
+    print("no esta en el dentro del radio o no  esta en el cuadrante 1 ")
