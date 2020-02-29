e49b123f0cabe61b0493fe23a142e5865b686c3e
jero98772 <jero98772@gmail.com>
2020-02-29 11:03:29 -0500

    tarea algoritmico

--- clases/clase#_8__pesnamineto_algorimico__2020-01-20/trabjo_inpractico/apartamento.py
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/trabjo_inpractico/apartamento.py
@@ -1,6 +1,10 @@
 import subprocess
-
-apts = 60
+apts = 2
+sumaEdad = 0
+residentesTotales = 0
+masculino = 0
+femenino = 0
+perros = 0
 for i in range(apts):
 	subprocess.run("clear", shell =True)
 	print("="*80)
@@ -25,22 +29,43 @@ for i in range(apts):
 	print("="*80)
 	residentes = int(input("buenas venimos a hacer un cesnso por parte del edficio nesesisto que me responda las siguietes pregutas \n ¿cuantas personas  en su vivienda viven en el apartamento?:  "))
 	if residentes > 0:
+		residentesTotales += residentes
 		mascota = str(input("tiene alguna especie de animal en su casa domesticada o una mascota \n P) <-- perr@ \n G) <-- gato \n >=>) <-- pez(es) \n O) <-- otra(s) \n responda: "))
+		if mascota.lower() == "p" or mascota.lower() == "perro" :
+			cantidadPerros = int(input("cuantos perros tiene usted en su residencia:  "))
+			perros += cantidadPerros  
 		for j in range(residentes):
-		    genero = str(input("¿usted es del genero ?\n M) <-- masculino \n F) <-- femenino \n I) <-- indefinido \n 404) <-- no encontrado \n responda: "))
-		    edad = int(input("si usted esta vivo a respondido lo anterior con sinceridad respononda esto con la mayor transparencia ¿cuantos años tiene?: "))
+		    genero = str(input("¿usted es del genero ?\n M) <-- masculino \n F) <-- femenino \n  responda: "))
+		    if  genero.lower() == "f" or genero.lower() == "femenino":
+		            femenino += 1
+		    elif genero.lower() == "m" or genero.lower() == "masculido":
+		            masculino += 1
+		    edad  = int(input("si usted esta vivo a respondido lo anterior con sinceridad respononda esto con la mayor transparencia ¿cuantos años tiene?: "))
 		    while not (0 < edad  < 150):
 		        print("esto es por el bien comun no ganas nada expto que el gran hermano no tenga tus verderos datos")
 		        edad = int(input("si usted esta vivo a respondido lo anterior con sinceridad respononda esto con la mayor transparencia ¿cuantos años tiene?: "))
-
 		    if input("¿si respondio con sinceridad ? \n si) <-- si respondi mayormente con  sinceridad \n no)<-- no perdoname la vida \n responda con cabesa: ").lower() == "no":
 		        while not (0 < edad  < 150):
 		            print("esto es por el bien comun no ganas nada expto que el gran hermano no tenga tus verderos datos")
 		            edad = int(input("si usted esta vivo a respondido lo anterior con sinceridad respononda esto con la mayor transparencia ¿cuantos años tiene? : "))
+			
 		
 		else :
+			sumaEdad += edad 
 			print("muy amale por su colaboracion no fue de mucha ayuda")
-			input("presione enter")
+			input("presione enter para continuar")
 
 	else :
-		print("corra l) <-- loco  \n f) <-- fantasma \n s) <-- un sueño \n e) <-- exeso de alcohol")
+		print("corra salvese l) <-- loco  \n f) <-- fantasma \n s) <-- un sueño \n e) <-- exeso de alcohol")
+promedioEdad = sumaEdad/apts
+#apts es la cantidad de eddades  y las veces a ejecutar solo si se termina el programa se logra hacer el promedio con la cantidad de veces que se ejucta para pedir la eddad
+print("="*80)
+print(f"numero de habitantes del edificioes: {residentesTotales}")
+print(f"existe un promedio de eddad de {promedioEdad} años")
+print(f"hay {perros} perr@s en el edificio")
+porcentajeTotal = (femenino+masculino)*100/100
+porcentajeF = 100*((femenino)*100/100)/2
+porcentajeM = 100*((masculino)*100/100)/2
+print(f"porcentajes de generos en el edificio \n de  mujeres {porcentajeF}%\n de  hombres {porcentajeM}%")
+
+
