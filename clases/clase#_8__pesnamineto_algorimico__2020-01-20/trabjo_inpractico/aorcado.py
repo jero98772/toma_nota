7c397e4e0da7505d684626511131dafd41982bea
jero98772 <jero98772@gmail.com>
2020-04-19 22:35:38 -0500

    aorcado y aogado

---
+++ clases/clase#_8__pesnamineto_algorimico__2020-01-20/trabjo_inpractico/aorcado.py
@@ -0,0 +1,246 @@
+# -*- coding: UTF-8 -*-
+import random
+import numpy as np
+import time
+import subprocess
+
+
+def palabra_aleatoria():
+    """
+    Genera una palabra aleatorio leida desde un archivo
+    :return: str la palabra generada
+    """
+
+    with open("words.txt", "r") as f:
+        data = f.readlines()
+
+    n = random.randint(0, len(data)-1)
+    return data[n].replace("\n","")
+
+
+def dibujar_ahorcado(partes):
+    """
+    Dibuja el muñeco del ahorcado con base en la partes dadas
+    :param partes: las partes del muñeco que se van a dibujar
+    1: cabeza
+    2: cabeza y cuerpo
+    3: cabeza, cuerpo y un brazo
+    4: cabeza, cuerpo y ambos brazos
+    5: cabeza, cuerpo, ambos brazo y una pierna
+    6: cabeza, cuerpo, ambos brazos y ambas piernas
+    """
+    print("{:_^10}".format(""))
+    print("{:>10}".format("|"))
+
+    if partes == 1:
+        print("{:>11}".format("(_)"))
+    elif partes == 2:
+        print("{:>11}".format("(_)"))
+        print("{:>10}".format("|"))
+        print("{:>10}".format("|"))
+        print("{:>10}".format("|"))
+    elif partes == 3:
+        print("{:>11}".format("(_)"))
+        print("{:>10}".format("|"))
+        print("{:>10}".format("/|"))
+        print("{:>10}".format("|"))
+    elif partes == 4:
+        print("{:>11}".format("(_)"))
+        print("{:>10}".format("|"))
+        print("{:>11}".format("/|\\"))
+        print("{:>10}".format("|"))
+    elif partes == 5:
+        print("{:>11}".format("(_)"))
+        print("{:>10}".format("|"))
+        print("{:>11}".format("/|\\"))
+        print("{:>10}".format("|"))
+        print("{:>10}".format("/ "))
+    elif partes == 6:
+        print("{:>11}".format("(_)"))
+        print("{:>10}".format("|"))
+        print("{:>11}".format("/|\\"))
+        print("{:>10}".format("|"))
+        print("{:>11}".format("/ \\"))
+
+    print("\n")
+def gameover():
+		print(" try again \n ... \n")
+		time.sleep(2)
+		print("  game over ")
+		time.sleep(1)
+		print("sorry men")
+		time.sleep(1)
+		for i in range(3):
+			print(". \n")
+			time.sleep(1)
+def adorno():
+	subprocess.run("clear", shell =True)
+	print('| .__________))______|          ')
+	print('| | / /      ||          ')
+	print('| |/ /       ||          ')
+	print('| | /        ||.-''.          ')
+	print('| |/         |/  _  \          ')
+	print('| |          ||  `/,|          ')
+	print('| |          (\\`_.           ')
+	print('| |         .-`-- .          ')
+	print('| |        /Y . . Y\          ')
+	print('| |       // |   | \\          ')
+	print('| |      //  | . |  \\          ')
+	print('| |     ()   |   |   (`         ')
+	print('| |          ||"||         ')
+	print('| |          || ||         ')
+	print('| |          || ||         ')
+	print('| |          || ||         ')
+	print('| |         / | | \         ')
+	print('""""""""""|_`-'   ' |"""|         ')
+	print('|"|"""""""\ \        "|"|         ')
+	print('| |        \ \        | |         ')
+	print(': :         \ \       : :            ')
+	print('. .          `""       . .         ')
+	print(" _                                             ")
+	print("| |                                            ")
+	print("| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  ")
+	print("| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ ")
+	print("| | | | (_| | | | | (_| | | | | | | (_| | | | |")
+	print("|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|")
+	print("                    __/ |                      ")
+	print("                   |___/                       ")
+	opciones = input("""
+(A)todo o Nada ,te sacrificas por adivinar la palabra completa
+(1) letra ,poner solo 1 letra 
+(C)rear palabra ,sirve para juegos multijugador
+(E)salir
+======================================================================
+(A) all or nothing, you sacrifice yourself for guessing the whole word
+(1) letter, put only 1 letter
+(C)reate word, good for multiplayer games
+(E)xit
+""").lower()
+	return opciones
+def adivinar(rndpalabra):
+		#print(rndpalabra)#shhhhhhh
+	palabracompleta = input("enter the word:  ").lower()
+	if str(rndpalabra) == str(palabracompleta):# las palabras traen un salto de linea por defecto
+		print("correct, the word was",rndpalabra)
+	else :
+		gameover()
+		print(rndpalabra+" and you say "+palabracompleta)
+def continuacion(opciones):
+	continuacion = input("play again?[Y/N] or play (O)ther:  ").lower()
+	if continuacion == "y":
+		return opciones, palabra_aleatoria() 
+	elif continuacion == "n":
+
+		exit()
+	elif continuacion == "o":
+		opciones = adorno()
+		return opciones ,palabra_aleatoria() 
+def win(acumulado,rndpalabra,count):
+	logro = ""
+	for win in acumulado:
+		logro+=win
+	if rndpalabra == logro:
+		name = input("Congratulations you win, Remember me your name?:   ")
+		print("""
+	                     __                     __                   _ 
+	 _   _  ___  _   _  | _|_      _____   _ __|_ |   __ _ _ __   __| |
+	| | | |/ _ \| | | | | |\ \ /\ / / _ \ | '_ \| |  / _` | '_ \ / _` |
+	| |_| | (_) | |_| | | | \ V  V / (_) || | | | | | (_| | | | | (_| |
+	 \__, |\___/ \__,_| | |  \_/\_( )___( )_| |_| |  \__,_|_| |_|\__,_|
+	 |___/              |__|      |/    |/     |__|                    
+	                         _                                                    
+	 _ __ ___ _ __ ___   ___| |__   ___ _ __   _   _  ___  _   _    __ _ _ __ ___ 
+	| '__/ _ \ '_ ` _ \ / _ \ '_ \ / _ \ '__| | | | |/ _ \| | | |  / _` | '__/ _ \   
+	| | |  __/ | | | | |  __/ |_) |  __/ |    | |_| | (_) | |_| | | (_| | | |  __/
+	|_|  \___|_| |_| |_|\___|_.__/ \___|_|     \__, |\___/ \__,_|  \__,_|_|  \___|
+	                                           |___/                              
+	         _                                             
+	  __ _  | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+	 / _` | | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+	| (_| | | | | | (_| | | | | (_| | | | | | | (_| | | | |
+	 \__,_| |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+	                           |___/                       
+	""",name, "you find", acumulado," in  hangman with",count ,"fails")
+	else:	
+		gameover()
+def templatejuego():
+	rndpalabra = palabra_aleatoria()
+	rndpalabra = rndpalabra.lower()
+	turn = True
+	count = 0
+	vidas = 6
+	logro = ""
+	arr = np.chararray(len(rndpalabra ))
+	acumulado = np.chararray(len(rndpalabra ),unicode=True)
+	arr[:] = " "
+	letrasfallos = ""
+	letras = [letras for letras  in  rndpalabra]
+	acumulado[:] =  "-"
+	opciones = adorno()
+	try:
+		opciones = opciones
+	except:
+		opciones = "1"
+	while turn:
+
+		if opciones == "e":
+			exit()
+			turn = False
+		while opciones == "c":
+
+			rndpalabra =input("enter the word so that your operator does not guess:  ").lower()
+			arr = np.chararray(len(rndpalabra ))
+			acumulado = np.chararray(len(rndpalabra ),unicode=True)
+			arr[:] = " "
+			letrasfallos = ""
+			letras = [letras for letras  in  rndpalabra]
+			acumulado[:] =  "-"
+			opciones = adorno()
+		while opciones == "a" :
+			adivinar(rndpalabra)
+			opciones , rndpalabra = continuacion(opciones)
+		while opciones == "1" and count<vidas :
+			print(letrasfallos)
+			print(acumulado)	
+			dibujar_ahorcado(count)	
+			if count == 0:
+				letra = input("what letter are you going to try ")
+				for i in range(len(letras)):
+					if  str(letra) == str(letras[i]):
+						acumulado[i] = letras[i]
+				else:		
+					#if   (str(letra)  in acumulado[i]):
+					if not( letra in letras):
+						count += 1
+						letrasfallos +=str(letra)
+
+
+			elif 0<count<vidas :
+				letra = input("what letter are you going to try ")
+				for i in range(len(letras)):
+					if  str(letra) == str(letras[i]):
+						acumulado[i] = letras[i]
+				else:		
+					#if   (str(letra)  in acumulado[i]):
+					if not( letra in letras):
+						count += 1
+						letrasfallos +="-"+str(letra)
+			
+			if  "-" not in acumulado:
+				win(acumulado,rndpalabra,count)
+				opciones , rndpalabra = continuacion(opciones)			
+			
+			
+
+		while not ( count<vidas) :	
+			print("game over")	
+			count = 0
+			logro = ""
+			arr = np.chararray(len(rndpalabra ))
+			acumulado = np.chararray(len(rndpalabra ),unicode=True)
+			arr[:] = " "
+			letrasfallos = ""
+			letras = [letras for letras  in  rndpalabra]
+			acumulado[:] =  "-"
+			opciones , rndpalabra = continuacion(opciones)	
+templatejuego()
