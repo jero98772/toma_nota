31aa12d2dd34bf58d45aa646b0860608fd1480e2
jero98772 <jero98772@gmail.com>
2020-08-12 09:28:16 -0500

    actividad cuenta huevos o guevos

---
+++ clases/clase#_15__poo_y_algoritmia__2020-08-03/caculadora.py
@@ -0,0 +1,12 @@
+from math import e ,pi ,sqrt ,exp ,log , cos, sin, tan, atan, asin ,acos
+class caculadora:
+	def __init__(self,expresion):
+		self.expresion = expresion
+	def calcular(self):
+		entrada=eval(self.expresion)
+		return entrada
+calculadora = caculadora(input("entre algo para calcular\n"))
+r = calculadora.calcular()
+print(r)
+
+
