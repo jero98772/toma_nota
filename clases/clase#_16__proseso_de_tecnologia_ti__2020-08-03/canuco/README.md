d4e2b48a5be079acb0d16b9129ac3da9c857345a
jero98772 <jero98772@gmail.com>
2020-08-15 22:22:39 -0500

    haciendo la tarea solo por que mis queridos compañeros no les gusta y no quieren colaborar por la nota de ellos y mi triste que se dependa de una nota :(

--- clases/clase#_16__proseso_de_tecnologia_ti__2020-08-03/canuco/README.md
+++ clases/clase#_16__proseso_de_tecnologia_ti__2020-08-03/canuco/README.md
@@ -28,5 +28,25 @@
                              |____V||  .  |
                                     |____V|
 # TODO list (aqui se pone las cosas que quedaron pendientes)
-hacer que las parejas no salgan cada cantidadFichas/2 [preterminadamente es cada des fichas 10]
-#### una de las librerias de generacion de numeros aleatorios fue desarrollada por jero98772                                
+- hacer que las parejas no salgan cada cantidadFichas/2 [preterminadamente es cada des fichas 10]
+
+- hacer que las fichas tengan un numero de referencia y con el que existan diferentes columnas y filas dependiendo de la cantiad de fichas selecionadas
+
+- introducir un manual de juego que pueda ver  el usuario en consola  antes de jugar o para aprender a jugar (mientras se llega a  escribir ,buscar --> y llege al codigo va a tener un aviso que dise "suponemos que usted sabe jugar canuco es como jugar un juego de memoria o concetrese etc... "  
+
+### una de las librerias de generacion de numeros aleatorios fue desarrollada por jero98772                                
+
+# algunas notas de desarrollo
+#### D--°> un ciclo para identificar las fichas
+se nesesita un ciclo para identificar las fichas que estan dentro un vector
+#### D--°> otro ciclo para los turnos
+para que se repita cuntas veces se ejecute el codigo hasta que se ecnutren todas las fichas
+#### o / y
+se puede optimisar  con un contador si se encuentran las fichas se quita o se rompe el programa mientras el codigo para  encontrar fichas esta dentro de una funcion
+
+#### D--°> pueden existir mas fichas que turnos ?
+si ,si se quiere dejar incompleto el juego 
+
+#### D--°> pueden existir mas turnos que fichas ?
+
+si ,si para que existan diferentes oportunidades de vulver a encontrar las fichas siempre que no vuelvan a exitir las oportunidades se acaben cuando se acaben las fichas
--- clases/clase#_16__proseso_de_tecnologia_ti__2020-08-03/canuco/concentrese.py
+++ clases/clase#_16__proseso_de_tecnologia_ti__2020-08-03/canuco/concentrese.py
@@ -1,10 +1,21 @@
 import numpy as np
 from datetime import datetime
 import time
+from pyfiglet import Figlet ,figlet_format
 from pseudorandom  import prandom # para matar algunas moscas por nuestra parte 
 import random # para matar otras moscas 
 #forma de las fichas [valor,identificador,posicion]
+def cantiadColumnas(num):# la idea es que nos diga cuantas columnas poner para las catas 	# probar  concatenado string
 
+	nuevoNum = 1
+	count = 0
+	multiplicador = 2
+	numMaxcolumnas = 3
+	while nuevoNum <= num and count <numMaxcolumnas:
+		count += 1
+		if nuevoNum*multiplicador <= num: 
+			nuevoNum = nuevoNum*multiplicador
+	return nuevoNum
 def banner():
     banner = """
     de ejm98 y jero98772
@@ -34,11 +45,72 @@ cartas y numeros = concentracion
                 |____V||  |  || \ / |
                        |____V||  .  |
                               |____V|
-
-                              """
+"""
     return banner
+
+def advertencias():
+	warnig = """
+====================================
+_____
+\ | /  suponemos que usted sabe jugar 
+ \O/   canuco es como jugar un juego                           
+  v    de memoria o concetrese etc... 
+_____  
+\ | /  si no entiende o no juega bien 
+ \O/   o pierde por la culpa del otro
+  v    le recomendamos que no juege  
+_____
+\ | /  revise el manual del juego
+ \O/   todavia no esta implementado
+  v    pero tu puedes implementarlo
+_____
+\ | /  diseñado unicamente y pensado 
+ \O/   para humanos favor no intente  
+  v    si es un animal 
+_____
+\ | /  nesesita  cabesa y saber pensar
+ \O/   para poder jugar
+  v  
+_____
+\ | /  no pierde nada con ayudar ,ayude 
+ \O/   en https://pastebin.com/JywQVZ3p
+  v    (era muy largo el hypervinculo)
+_____
+\ | /  si quiere contribuir , contribuya
+ \O/   si quiere donar , done aqui 
+  v tb1qgt4trnnsxdmfcpnydkmk8ht0y4esg4cdvzmhaz
+_____
+\ | /  este proyecto es codigo libre ve uds 
+ \O/   quien(es) trabajo en este hypervinculo 
+  v    https://pastebin.com/JywQVZ3p
+_____
+\ | /  si por algun motivo se equivoco y
+ \O/   y descargo esto dese la oportunidad 
+  v    para jugar 
+_____
+\ | / juege disfrutando si no ,no juege
+ \O/ 
+  v  
+_____
+\ | / porfavor no critique 
+ \O/  disfrute y
+  v   colabore
+_____
+\ | / si no quiere leer no lea
+ \O/  :)
+  v   el juego se puede juagar solo con enter
+_____
+\ | / si quiere paselo a ingles
+ \O/  si no entiende lo que dice paselo
+  v   a lo que entienda (y pase lo que hiso)
+"""	
+	return warnig
+# 
+
 def main():
+	f = Figlet()
 	print(banner())
+	print(advertencias())
 	valoresFichas = 3
 	segundosEnUnMinuto = 60
 	cantidadFichas = input("introsca la cantidad de fichas para jugar preferible mente par el valor que va  tener por default es 20\n")
@@ -51,11 +123,10 @@ def main():
 		cantidadFichas = 20
 	if not cantidadFichas :
 		cantidadFichas = 20	
-	#now = int(datetime.now().strftime('%S'))
-	now = 0
-	if now == 0:
-		now = random.randrange(1,60)#not now
-	r = prandom(valorInicial = now,incrementador = now ,multiplicador = 1, mod = cantidadFichas/2,veces = cantidadFichas)
+	now = int(datetime.now().strftime('%S'))
+	if now <= 10:
+		now = random.randrange(10,60)#not now
+	r = prandom(valorInicial = now,incrementador = 7 ,multiplicador = 1, mod = cantidadFichas/2,veces = cantidadFichas)
 	fichas = np.zeros((cantidadFichas,valoresFichas))
 	valorfichas = np.asarray(r.vector())
 	dibujofichas ="""
@@ -68,5 +139,29 @@ def main():
 	"""
 	for i in range(1,cantidadFichas+1): 
 		print(dibujofichas+str(i))
-	#print(valorfichas) 
+	fichasRestantes = 0
+	count = 0
+	while fichasRestantes < cantidadFichas:
+
+		eleccion = input("escoja la pocion a la que corresponde una ficha [default=?/aleatorio]")
+		try:
+			eleccion = int(eleccion)
+		except:
+			eleccion = random.randrange(0,cantidadFichas/2)
+		if not eleccion:
+			eleccion = random.randrange(0,cantidadFichas/2)
+		print(f.renderText(str(valorfichas[eleccion])))
+		eleccion2 = input("escoja la pocion a la que corresponde una ficha [default=?/aleatorio]")
+		try:
+			eleccion2 = int(eleccion2)
+		except:
+			eleccion2 = random.randrange(0,cantidadFichas/2)
+		if not eleccion:
+			eleccion2 = random.randrange(0,cantidadFichas/2)
+		print(f.renderText(str(valorfichas[eleccion2])))
+		#for i in range(1,cantidadFichas+1):
+			#print(f.renderText(str(valorfichas[i-1]))+str(i))
+		print(eleccion)
+		print(valorfichas)
+		quit()
 main()
