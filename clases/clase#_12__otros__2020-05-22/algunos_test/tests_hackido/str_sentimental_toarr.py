d13c54646878febd15c5bf79823a738ccd2b5a9f
jero98772 <jero98772@gmail.com>
2020-08-29 15:45:26 -0500

    correcion  de no poner la respiuesta con su debido analisis de la tarea de matematicas discretas que se entrega mañana a las 12 pm

---
+++ clases/clase#_12__otros__2020-05-22/algunos_test/tests_hackido/str_sentimental_toarr.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-"
+"""
+str_sentimental_toarr - 2020 - por jero98772
+str_sentimental_toarr - 2020 - by jero98772
+"""
+sentimeintos = """*** estres
+*** Tristeza
+*** depresión 
+*** ansiedad
+*** miedo
+*** Fobia
+*** Llanto frecuente
+*** Sentimientos de inferioridad 
+*** ira
+*** agresividad
+*** ideación suicida
+*** Alegria 
+*** felicidad
+*** emoción
+*** Amor
+*** odio
+*** paz interior
+*** armonia
+*** intranquilidad
+*** Amargado
+*** engreido
+*** desconocimiento
+*** conocimiento
+*** incomprension
+*** compresnion
+*** extraño
+*** timidez
+*** aislarse
+*** preocupado
+*** aislado
+*** soledad
+*** intmidado
+*** agresion
+"""
+larrySentimental = []
+sentimiento = ""
+sentimeintos = sentimeintos.replace("*** ","-")
+sentimeintos = sentimeintos.replace("\n","")
+for i in sentimeintos:
+	if i == "-":
+		larrySentimental.append(sentimiento)
+		sentimiento = ""
+	elif i == 0:
+		pass
+	else :
+		sentimiento += i
+print(larrySentimental)
+#un log como ['estres', 'Tristeza', 'depresión ', 'ansiedad', 'miedo', 'Fobia', 'Llanto frecuente', 'Sentimientos de inferioridad ', 'ira', 'agresividad', 'ideación suicida', 'Alegria ', 'felicidad', 'emoción', 'Amor', 'odio', 'paz interior', 'armonia', 'intranquilidad', 'Amargado', 'engreido', 'desconocimiento', 'conocimiento', 'incomprension', 'compresnion', 'extraño', 'timidez', 'aislarse', 'preocupado', 'aislado', 'soledad', 'intmidado']
+
