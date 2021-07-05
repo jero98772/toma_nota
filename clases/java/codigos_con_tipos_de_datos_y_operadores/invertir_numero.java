902d4ec0c42d60e732e035eed7e84a2c69aedec5
jero98772 <jero98772@protonmail.com>
2021-07-05 14:46:43 -0500

    hacer ejercio de invertir numero de 3 digitos

--- clases/java/codigos_con_tipos_de_datos_y_operadores/invertir_numero.java
+++ clases/java/codigos_con_tipos_de_datos_y_operadores/invertir_numero.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class invertir_numero {
     public static void main(String[] args){
-        
+        System.out.println("Ingrese un numero de 3 digitos");
+        Scanner input = new Scanner(System.in);
+        short num = input.nextShort();
+        if (99<num && num<1000){
+            int unidades = num/100;
+            int decenas = ((num/10)%10)*10;
+            int centenas = (num*100)%1000;
+            int newnum = centenas+decenas+unidades;
+            System.out.println("el numero ingresado fue: "+num+"\n y el numero invertido es: "+newnum);
+        }
+        else{
+            System.out.println("porfavor ingrese un numero de 3 digitos como 666");
+        }
     }
 }
+
