bb4b73363102c366467b1fd783d063a65a40b6e6
jero98772 <jero98772@protonmail.com>
2021-07-29 15:41:30 -0500

    crear hipogago

--- clases/java/codigos_con_tipos_de_datos_y_operadores/Hipotenusa.java
+++ clases/java/codigos_con_tipos_de_datos_y_operadores/Hipotenusa.java
@@ -1,3 +1,6 @@
+//codigo libre
+//licencia de Creative Commons Reconocimiento 4.0 Internacional
+//por jero98772
 import java.util.Scanner;
 public class hipotenusa {
     public static void main(String[] args){
@@ -7,7 +10,11 @@ public class hipotenusa {
         float a = input.nextFloat();
         System.out.println("ingrese un numero b");
         float b = input.nextFloat();
-        float hipotenusa = pow(pow(a,2)+pow(b,2),1/2)
-
+        //float hipotenusa = (a²+b²)**¹/²; ((a*a)+(b*b))
+        //float hipotenusa = Math.pow(Math.pow(a,2.0f)+Math.pow(b,2.0f),1f/2f);
+        double hipotenusa = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.println("aplicando la hipotenusa de √(a²+b²) a "+a+" y "+b+" se :");
+        System.out.println(hipotenusa);
+        System.out.println("hi"+"po".repeat((int) hipotenusa)+"tenusa");
     }
 }
