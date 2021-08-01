f1536a4be4a50a6a607b3b199dee2a2a4eca0a67
jero98772 <jero98772@protonmail.com>
2021-08-01 00:27:31 -0500

    add contadorsimple...

---
+++ clases/java/obetos_y_clases/ContadorSimple.java
@@ -0,0 +1,14 @@
+public class ContadorSimple{
+    int conteo = 1;
+    public void aumentar(){
+        this.conteo += 1;
+    }
+    public void disminuir(){
+        this.conteo -= 1; 
+    }
+    public int getConteo(){
+        return this.conteo;
+    }
+    //public static void main(String[] args){}
+
+}
