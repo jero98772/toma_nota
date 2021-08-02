80a6b2fc85061b9198c98ba8a8def316dc1d0452
jero98772 <jero98772@protonmail.com>
2021-08-02 17:19:11 -0500

    add mastotasimple.java

---
+++ clases/java/obetos_y_clases/MascotaSimple.java
@@ -0,0 +1,20 @@
+public class MascotaSimple{
+    private int eddad =8;
+    private string nombre;
+    public static void main(){
+        nombre = Kitty;
+    }
+    public static void setNombre(string nuevoNombre){
+        nombre = nuevoNombre
+    }
+    public static void setMascota(string nuevoNombre,int nuevaEdad){
+        nombre = nuevoNombre;
+        edad = nuevaEdad;
+    }
+    public static String getNombre(){
+        return nombre;
+    }
+    public static int getEdad(){
+        return edad;
+    }
+}
