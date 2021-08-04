c8289a0c6bd3231cce089ad16a43ece043a262b4
jero98772 <jero98772@protonmail.com>
2021-08-04 16:05:34 -0500

    dd juego

---
+++ clases/java/obetos_y_clases/juego/Jugador.java
@@ -0,0 +1,16 @@
+public class Jugador{
+    private string nombre;
+    private int sauld;
+    public void setNombre(String n){
+        this.nombre = n;
+    }
+    public setSalud(int s){
+        this.salud = s;
+    }
+    public String getNombre(){
+        return nombre
+        }
+    public int getSalud(){
+        
+    }
+}
