9d8b876c213aae45a5b4187d77b40514a8953de1
jero98772 <jero98772@protonmail.com>
2021-08-12 20:38:28 -0500

    add some sthi . java

---
+++ clases/java/ciclos/Mate.java
@@ -0,0 +1,10 @@
+//import java.util.Scanner;
+public class Mate{//
+    int factorial(int num) {
+        int resultado = 1;
+        for (int i=1;i<num;i++){
+            resultado = resultado *i;
+                }
+        return resultado;
+        }
+}
