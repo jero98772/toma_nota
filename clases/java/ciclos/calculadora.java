735424a1e0dba71d2a30ff75da6690d2d9456ecb
jero98772 <jero98772@protonmail.com>
2021-08-14 10:44:03 -0500

    update shit . java

--- clases/java/ciclos/calculadora.java
+++ clases/java/ciclos/calculadora.java
@@ -1,24 +1,36 @@
-import java.ulit.Scanner
+import java.util.Scanner;
 public class calculadora{
 	public static void main(String[] args){
 		String opcion; 
+		int num1,num2;
 		boolean continuar = true;
 		Scanner input = new Scanner(System.in);
 		while(continuar){
-			System.out.println("escoja una de las siguintes opciones:\n\tsuma\t sume 2 numeros");
-			opcion = input.nextString();
-			System.out.println("ingrese el nuemro 1");				
-			int num1 = input.nextInt();
-			System.out.println("ingrese el nuemro 1");				
-			int num2 = input.nextInt();
+			System.out.println("escoja una de las siguintes opciones:\n\tsuma\t sume 2 numeros\n\tresta\t reste 2 numeros\n\tcuadrado\tsace el cuadrado de un numero\n\tsalir\t chao pescao");
+			opcion = input.next();
 			switch(opcion){
 				case "suma":
-					System.out.println(num1+num2)
-					break
+					System.out.println("ingrese un numero");				
+					num1 = input.nextInt();
+					System.out.println("ingrese otro numero");				
+					num2 = input.nextInt();
+					System.out.println(num1+num2);
+					break;
 				case "resta":
-					break
-				case "multiplicacion":
-			
+					System.out.println("ingrese un numero");				
+					num1 = input.nextInt();
+					System.out.println("ingrese otro numero");				
+					num2 = input.nextInt();
+					System.out.println(num1-num2);
+					break;
+				case "cuadrado":
+					System.out.println("ingrese un numero");				
+					num1 = input.nextInt();
+					System.out.println(num1*num1);
+					break;
+				case "salir":
+					System.out.println("chao 🐟️");
+					break;			
 			} 
 		}
 	}
