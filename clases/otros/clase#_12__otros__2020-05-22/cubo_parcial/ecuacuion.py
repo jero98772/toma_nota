a90df20f6ceefe2384fce8ea6e97b28ba9df884c
jero98772 <jero98772@protonmail.com>
2021-08-25 12:39:25 -0500

    add analisis textual y fisnis ecuacion cubo dot py

--- clases/otros/clase#_12__otros__2020-05-22/cubo_parcial/ecuacuion.py
+++ clases/otros/clase#_12__otros__2020-05-22/cubo_parcial/ecuacuion.py
@@ -1,14 +1,16 @@
 print("introdusca las dimenciones del cubo")
-x = input("x")
-y = input("y")
-z = input("z")
-#puntos = [[0,0,0],[0,0,z],[0,y,0],[x,0,0],[0,y,z],[x,0,z],[x,y,0],[x,y,z]]
-print("introsuca un punto")
-xp = input("x")
-yp = input("y")
-zp = input("z")
+#se asume que el usuario solo ingresara numeros naturales mayores a 0
+x = int(input("x"))
+y = int(input("y"))
+z = int(input("z"))
+print("introdusca un punto")
+xp = int(input("x"))
+yp = int(input("y"))
+zp = int(input("z"))
 dot = [xp,yp,zp]
-plain = []
-def genPlano(dot,cubeAxisPlain):
-	d=a*x+b*y+c*z
-	hippo = ((a**2+b**2+c**2)**(1/2))
+cube = [x,y,z]
+def isIn(dot,cube):
+	#asumiendo que el cubo esta ubicado en 0,0,0
+	if -1*(cube[0]/2)<dot[0]<cube[0]/2 and -1*(cube[1]/2)<dot[1]<cube[1]/2 and -1*(cube[2]/2)<dot[2]<cube[2]/2:print("el punto esta adentro del cubo")
+	else:print("el punto no esta adentro del cubo")
+isIn(dot,cube)
