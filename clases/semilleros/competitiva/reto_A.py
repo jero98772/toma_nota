f9ab2ebb6a96424b8a406e1be15ce9bd3e4c5b23
jero98772 <jero98772@protonmail.com>
2021-08-28 15:36:48 -0500

    add semillero

---
+++ clases/semilleros/competitiva/reto_A.py
@@ -0,0 +1,17 @@
+#oficina de
+larry = []
+t = int(input("cantidad\n"))
+if 1<t<2000:
+	for i in range(t):
+		numero = input("prefijo + numero")
+		larry.append(numero)
+def esPrefijo(array):
+	contador = 0
+	prefijos = []
+	for i in array:
+		for ii in array:
+			print(i,ii,str(ii)[:len(i)])
+			if str(ii)[:len(i)] in i   and str(ii)[:len(i)] != i:#
+				contador += 1
+	return contador	
+print(esPrefijo(larry))
