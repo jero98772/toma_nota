13c825a82a6e9c06014e57f01f208808c3d562be
jero98772 <jero98772@protonmail.com>
2021-08-30 19:47:16 -0500

    update competiva

---
+++ clases/semilleros/competitiva/reto_A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  int num,t,resultado;
+
+  cout <<"introduce la cantidad de telefonos"<<endl;
+  cin>>t;
+  if(1<=t<=2000){
+	string telefonos[t]={};
+	for(int i = 0;i<t;i++){
+	
+	}   
+  }else{
+     cout<<"la cantidad de telefonos no es valida"<<endl;
+  }
+  cout<<"el resulatdo es: "<<resultado <<endl;
+  return 0;
+}
