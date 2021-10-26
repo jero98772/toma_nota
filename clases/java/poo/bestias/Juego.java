083517d07d71ec579a9dbdc5ec05b8c138813c82
jero98772 <jero98772@protonmail.com>
2021-10-26 11:27:47 -0500

    finish moddle vps shit

--- clases/java/poo/bestias/Juego.java
+++ clases/java/poo/bestias/Juego.java
@@ -1,8 +1,18 @@
 public class Juego{
+    private Monstruo[] monstruos=new Monstruo[4];
     public Juego(){
-        Monstruo[] monstruos=new Monstruo[4];
-        monstruos[0]=Orco("Garnag",10,"grrr");
-        monstruos[1]=Orco("Rogthun",5,"purr");
-        monstruos[2]=Orco("Brenton",20,"metal");
+        Orco orco1=new Orco("Garnag",10,"grrr");
+        Dragon dragon1=new Dragon("Brenton",20,"metal");
+        Orco orco2=new Orco("Rogthun",5,"purr");
+        Dragon dragon2=new Dragon("Shenlong",40,"cuero");
+        monstruos[0]=orco1;
+        monstruos[1]=dragon1;
+        monstruos[2]=orco2;
+        monstruos[3]=dragon2;
+    }
+    public void moverMonstruos(){
+        for(int i=0;i<monstruos.length;i++){
+            monstruos[i].moverse();
+        }
     }
 }
