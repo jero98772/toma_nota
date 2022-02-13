8ca66e1cdcfdc222770d8c159e10c5c56f048b54
jero98772 <jero98772@protonmail.com>
2022-02-13 08:35:27 -0500

    add contest and dsa fil

--- clases/estructuras_de_datos_y_algoritmos/tareas/recursion/conejo.py
+++ clases/estructuras_de_datos_y_algoritmos/tareas/recursion/conejo.py
@@ -1,12 +1,18 @@
 def walkAux(board):
 
-def walk(xdes,ydes,board)
+def walk(xdes,ydes,board,score):
+    if xdes>=len(borad) and ydes>=len(borad):return score 
+        if xdes>=len(borad)
+    else:
+      #tmp puntos actuales
+      if score>tmp:
+        return walk(xdes,ydes,board,tmp)
 def main():
     n,m=input().split(" ")
     c,a=input().split(" ")
     carrot="#"#input("carrot character")
     apple="*"#input("apple caharcter")
-    {".":0,carrot:c,apple:a}
+    scoreTable={".":0,carrot:c,apple:a}
     board=[]
     for i in range(n):
         tmp=input().split(" ")
