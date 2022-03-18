abfcc1c0d6bf6fb6fa397db464335558d4e4551c
jero98772 <jero98772@protonmail.com>
2022-03-18 11:34:35 -0500

    update linked list

--- clases/estructuras_de_datos_y_algoritmos/tareas/ds/linklist/main_linked_list.py
+++ clases/estructuras_de_datos_y_algoritmos/tareas/ds/linklist/main_linked_list.py
@@ -28,11 +28,12 @@ class linkedList:
                 self.head=newNode
     def getData(self):
         data=""
-        current=self.headx
+        current=self.head
         while current.next:
             data+=" "+str(current.val)
             current=current.next
-        current.next=newNode
+        return data
+        #current.next=newNode
 
     def search(self, itemsearch):
         current=self.head
@@ -43,7 +44,19 @@ class linkedList:
             current = current.next
          
         return False
-
+    def insert(self, x, data):
+        n = self.head
+        print(n.next)
+        while not n :
+            if n.val == x:
+                break
+            n = n.ref
+        if not n :
+            print("item not in the list")
+        else:
+            new_node = Node(data)
+            new_node.ref = n.ref
+            n.ref = new_node
  
  
 def recSearch(node,itemsearch):
@@ -66,15 +79,21 @@ def recPos(node,itemsearch,pos):
         else:
             return recPos(current.next,itemsearch,pos+1)
 
-def recInsert(node,item,pos,posobj):
+def recInsert(head,node,item:node,posobj,pos):
+    #print(posobj,pos)
     current=node
     if not current:
         return -1#not found
     else:
         if pos==posobj:
-            return pos
+            tmp=current.next
+            item.next=tmp
+            current.next=item   
+            return head  
+        elif posobj==0:
+             item.next=head
         else:
-            return recPos(current.next,item,,pos+1)
+            return recInsert(head,current.next,item,posobj,pos+1)
 
 def max(node,maxnum):
     current=node
@@ -88,12 +107,16 @@ def max(node,maxnum):
 
 def test():
     ll=linkedList()
-    ll.madd(1,2,3,6,5,7,8,4)
+    ll.madd(1,2,3,6,5,7,8)
     #print(ll.search(4).val)
     #tmp=recSearch(ll.head,4)
     #print(tmp.val)
-    print(recPos(ll.head,4,0))
-    print(recInsert(ll.head,4,3,0))
+    #print(recPos(ll.head,4,0))
+    recInsert(ll.head,ll.head,node(4),0,0)
+    recInsert(ll.head,ll.head,node(4),0,0)
+    recInsert(ll.head,ll.head,node(4),0,0)
+
+    print(ll.getData())
 
 
 test()
