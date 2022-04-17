0b92b0df38844c4797c10f561a96089de74e2c8e
jero98772 <jero98772@protonmail.com>
2022-04-17 08:11:21 -0500

    update

---
+++ clases/estructuras_de_datos_y_algoritmos/tareas/dicts/1.py
@@ -0,0 +1,10 @@
+class node():
+    def __init__(self,value, weight):
+        self.value=value
+        self.weight=weight
+        self.next=None
+a=node(123,12)
+b=node(123,12)
+c=node(123,12)
+#dict={"a":,}
+if 
