e8b84a80e4aa089aed9be35567ddb6bde44e87d8
jero98772 <jero98772@protonmail.com>
2022-07-25 19:29:49 -0500

    update

--- clases/datos2/n-reinas.cpp
+++ clases/datos2/n-reinas.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 int main(){
 	int n;cin>>n;
+	int board[n][n];
 	if(n<4){
 	 	cout<<n<<"n invalido";
+	}else{
+		//add defautl value for matrix, if you create matrix in main() or other function normal values will be random, but if you create out of function was fill with 0 
+		for(int i=0;i<n;i++){
+				for(int ii=0;ii<n;ii++){
+					board[i][ii]
+			}
+		}
 	}
 }
 //https://www.cs.buap.mx/~zacarias/FZF/nreinas3.pdf
+//https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/
