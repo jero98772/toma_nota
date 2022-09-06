5b292de7e6e6d7f047ea1a81af211e9268f94644
jero98772 <jero98772@protonmail.com>
2022-09-05 21:37:52 -0500

    update

--- clases/semilleros/biologia_comp/reto2/main.py
+++ clases/semilleros/biologia_comp/reto2/main.py
@@ -1,31 +1,63 @@
 import os
-
+"""
+f-> AGTAATT
+R-> CATGAGTGACGTT
+Rc->TCATTAACT
+"""
 def reverse(bases,sequences):
 	Rc=sequences[::-1]
 	R=""
 	for i in range(len(sequences)):
+		#print(Rc[i],"\n",Rc[i])
 		R+=bases[Rc[i]]
 	return R,Rc	
 def readtxt(name):
 	"""
 	readtxt(name) , return txt content as array ,element by line 
 	"""
-	content = []
+	content = ""
 	with open(name, 'r') as file:
 		for i in file.readlines():
-			content.append(str(i).replace("\n",""))
+			content+=str(i).replace("\n","").replace(" ","")
 	return content
+def compare(sequence1,sequence2):
+	newsequence=""
+	if sequence1=="":
+		return sequence2
+	if sequence2=="":
+		return sequence1
+	for i in range(min(len(sequence1),len(sequence2))):
+		if sequence1[i]==sequence2[i]:
+			newsequence+=sequence1[i]
+		else:
+			newsequence+="_"
+	return newsequence
 
 def main():
 	folder="data/"
 	bases={"A":"T","T":"A","G":"C","C":"G"}
 	folders=os.listdir(folder)
 	sequences=[]
+	frist=True
+	totalsequence=""
+	f=""
+	Rc=""
 	for i in folders:
-		sequence=readtxt(name)
-		if "No_paso2":
+		print(i)
+		sequence=readtxt(folder+i)
+		if "NOPASO" in i:
+			f=sequence
 		else:
 			R,Rc=reverse(bases,sequence)
+			#print(R,Rc)
+		if frist and Rc!="":
+			print(R,"\n",f)
+			totalsequence=compare(f,R)
+			frist=False
+		else:
+			new=compare(f,Rc)
+			totalsequence=compare(totalsequence,new)
+	print(totalsequence)
 
 if __name__ == '__main__':
 	main()
